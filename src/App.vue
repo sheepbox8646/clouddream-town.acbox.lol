3ffc900f88c0ca59316b4a219b94c6af5573f11e
Acbox Liu <850625057@qq.com>
2024-02-07 02:02:14 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,63 @@
 </script>
 
 <template>
+  <ul class="guider">
+    <li><a href="#home">主页</a></li>
+    <li><a href="#news">新闻</a></li>
+    <li><a href="#contact">联系</a></li>
+    <li><a href="#about">关于</a></li>
+  </ul>
   <div class="back">
-    wwww
-    <!-- <img src="./assets/logo.png" class="logo" alt="logo" /><br /> -->
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <img src="./assets/logo.png" class="logo" alt="logo" /><br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
   </div>
 </template>
 
@@ -19,13 +73,26 @@
 }
 
 .logo:hover {
-  filter: drop-shadow(0 0 2em #8ff666aa);
+  filter: drop-shadow(0 0 2em #ffdd00);
 }
 
 .back {
-  background-image: url('./background.png');
+  background-image: url('./assets/background.png');
   margin: 0;
   background-size: 100%, 100%;
   background-attachment: fixed;
+  width: 100%;
+  height: 100%;
+}
+
+.guider {
+  list-style-type: none;
+  position: fixed;
+}
+
+.guider li {
+  float: right;
+  margin: 20px;
+  height: 40px;
 }
 </style>
